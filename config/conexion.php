af3a095590a8348b19c3a2ea0abe934a84776d38
Diego Armando Rios <diego010280@hotmail.com>
2025-05-29 14:28:06 -0300

    pri

---
+++ config/conexion.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+
+    class Conectar{
+        protected $dbh;
+
+        protected function conexion() {
+        try {
+            $conectar= $this->dbh = new PDO("mysql:host=localhost;dbname=andercode_helpdesk", "root", "");
+            $conectar->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conectar;
+        } catch (PDOException $e) {
+            echo "Error de conexión: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    }
+
+    public function set_names() {
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }
+
+    public function ruta(){
+        return 'http://localhost/helpdesk/';
+    }
+}
+
+
+?>
